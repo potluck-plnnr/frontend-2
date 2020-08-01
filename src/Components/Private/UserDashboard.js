ce6e82a0ab53b8dc0ffa477cc7c63edfdf594ea6
Tay8472 <tpm8472@gmail.com>
2020-08-01 15:16:15 -0600

    hopefully final

--- src/Components/Private/UserDashboard.js
+++ src/Components/Private/UserDashboard.js
@@ -4,19 +4,26 @@ import { useHistory } from "react-router-dom";
 
 import List from "./PotluckListAll";
 
+// import GuestForm from "./GuestForm";
+import Potluck from "../../Potluck";
+
 let PotluckDashboard = () => {
   let history = useHistory();
 
-const logOut = () => {
-  localStorage.clear();
-  history.push("/");
-};
+  const logOut = () => {
+    localStorage.clear();
+    history.push("/");
+  };
 
   return (
     <div>
       <List />
       {/* GUESTFORM HERE */}
       {/* POTLUCK HERE */}
+
+      {<button onClick={Potluck}>Create</button>}
+      {/* {<button onClick={GuestForm}>GuestForm</button>} */}
+
       <button onClick={logOut}>Log Out</button>
     </div>
   );
